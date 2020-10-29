ecd5d0424e406470caad100e089f12f42b4a44f0
Sahar Jafari <shr.jafari@gmail.com>
2020-10-28 20:35:21 -0700

    Setup server + User endpoints

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-// code away!
+const server = require("./server");
+
+server.listen(5000, () => console.log("API running on port 5000"));
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,33 +1,106 @@
-const express = require('express');
+const express = require("express");
+
+const userDb = require("./userDb");
+const Posts = require("../posts/postDb");
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
-  // do your magic!
+// router.use((req, res, next) => {
+//   console.log("inside the user router");
+//   next();
+// });
+
+router.post("/", (req, res) => {
+  userDb
+    .insert(req.body)
+    .then((hub) => {
+      res.status(201).json(hub);
+    })
+    .catch((error) => {
+      // log error to server
+      console.log(error);
+      res.status(500).json({
+        message: "Error adding the user",
+      });
+    });
 });
 
-router.post('/:id/posts', (req, res) => {
-  // do your magic!
+router.post("/:id/posts", (req, res) => {
+  const post = { user_id: parseInt(req.params.id), ...req.body };
+  console.log(post);
+  Posts.insert(post)
+    .then((resp) => {
+      res.status(201).json(resp);
+    })
+    .catch((err) => {
+      res.status(404).json({
+        message: "Posts for the specified userID does not exist.",
+      });
+    });
 });
 
-router.get('/', (req, res) => {
-  // do your magic!
+router.get("/", (req, res) => {
+  userDb.get().then((users) => {
+    res.status(200).json(users);
+  });
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  userDb.getById(id).then((user) => {
+    if (!user) {
+      res.status(400).json({
+        message: "User with the specified ID does not exist.",
+      });
+    } else {
+      res.status(200).json(user);
+    }
+  });
 });
 
-router.get('/:id/posts', (req, res) => {
-  // do your magic!
+router.get("/:id/posts", (req, res) => {
+  const { id } = req.params;
+  userDb.getUserPosts(id).then((posts) => {
+    if (!posts) {
+      res.status(400).json({
+        message: "The posts for the specified user ID does not exist.",
+      });
+    } else {
+      res.status(200).json(posts);
+    }
+  });
 });
 
-router.delete('/:id', (req, res) => {
-  // do your magic!
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  userDb
+    .remove(id)
+    .then((resp) => {
+      res.status(200).json({
+        message: "The user is deleted.",
+      });
+    })
+    .catch((err) => {
+      res.status(404).json({
+        message: "The user with the specified ID does not exist.",
+      });
+    });
 });
 
-router.put('/:id', (req, res) => {
-  // do your magic!
+router.put("/:id", (req, res) => {
+  const { id } = req.params;
+  userDb
+    .update(id, req.body)
+    .then((resp) => {
+      res.status(200).json({
+        message: "The user is updated.",
+      });
+    })
+    .catch((err) => {
+      res.status(404).json({
+        message: "The user with the specified ID does not exist.",
+      });
+    });
 });
 
 //custom middleware
